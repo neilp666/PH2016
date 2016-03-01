8a26d2f5056c65adab9b0b95cc4f09b9768d94e7
neil patel <neil@myindianweddingapp.com>
2016-03-01 12:41:59 +0000

    Added current_user method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,11 @@ class SessionsController < ApplicationController
   def create
     envhash = env["ominauth.outh"]
 
-    if user = User.where(envhash.slice("provider", "uid")).first
-      user.update_attributes("provider" => envhash["provider"], "uid" => envhash["uid"], "name" => 
-        envhash["info"]["name"], "handle" => envhash["info"]["nickname"], "image" => envhash["info"]["image"])
+    if user = User.where(envhash.slice("provider", "uid").permit(:uid)).first
+
+      user.update_attributes("provider" => envhash["provider"], "uid" => envhash["uid"], "name" => envhash["info"]["name"], "handle" => envhash["info"]["nickname"], "image" => envhash["info"]["image"])
     else
-      user = User.new("provider" => envhash["provider"], "uid" => envhash["uid"], "name" => envhash["info"]["name"],
-       "handle" => envhash["info"]["nickname"], "image" => envhash["info"]["image"])
+      user = User.new("provider" => envhash["provider"], "uid" => envhash["uid"], "name" => envhash["info"]["name"], "handle" => envhash["info"]["nickname"], "image" => envhash["info"]["image"])
       user.save
     end
 
